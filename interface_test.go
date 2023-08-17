935fffcf1a0ec13d9df4ea23870792cf0a868ef6
Medet Ahmetson <admin@blocklords.io>
2023-08-17 21:30:08 +0300

    fix the lint in the interface testing

--- interface_test.go
+++ interface_test.go
@@ -101,5 +101,5 @@ func (suite *TestInterfaceSuite) TestRun() {
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
 func TestInterface(t *testing.T) {
-	suite.Run(t, new(TestEnvSuite))
+	suite.Run(t, new(TestInterfaceSuite))
 }
