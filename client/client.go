223ed0972222feb91db19647e597f82d439459c4
Medet Ahmetson <ahmetson@zoho.com>
2023-09-03 15:58:28 +0400

    Add a client that can get config from the handler

---
+++ client/client.go
@@ -0,0 +1,217 @@
+package client
+
+import (
+	"fmt"
+	"github.com/ahmetson/client-lib"
+	clientConfig "github.com/ahmetson/client-lib/config"
+	"github.com/ahmetson/common-lib/data_type/key_value"
+	"github.com/ahmetson/common-lib/message"
+	"github.com/ahmetson/config-lib"
+	"github.com/ahmetson/config-lib/handler"
+	handlerConfig "github.com/ahmetson/handler-lib/config"
+	"time"
+)
+
+type Client struct {
+	socket *client.Socket
+}
+
+func New() (*Client, error) {
+	configHandler := handler.SocketConfig()
+	socketType := handlerConfig.SocketType(configHandler.Type)
+	c := clientConfig.New("", configHandler.Id, configHandler.Port, socketType).
+		UrlFunc(handlerConfig.ExternalUrlByClient)
+
+	socket, err := client.New(c)
+	if err != nil {
+		return nil, fmt.Errorf("client.New: %w", err)
+	}
+
+	return &Client{socket: socket}, nil
+}
+
+func (c *Client) Close() error {
+	if c.socket != nil {
+		return c.socket.Close()
+	}
+
+	return nil
+}
+
+func (c *Client) Timeout(duration time.Duration) *Client {
+	c.socket.Timeout(duration)
+	return c
+}
+
+func (c *Client) Attempt(attempt uint8) *Client {
+	c.socket.Attempt(attempt)
+	return c
+}
+
+func (c *Client) Service(id string) (*config.Service, error) {
+	req := message.Request{
+		Command:    handler.ServiceById,
+		Parameters: key_value.Empty().Set("id", id),
+	}
+
+	rep, err := c.socket.Request(&req)
+	if err != nil {
+		return nil, fmt.Errorf("socket.Request('%s'): %w", handler.ServiceById, err)
+	}
+
+	if !rep.IsOK() {
+		return nil, fmt.Errorf("replied an error: %s", rep.Message)
+	}
+
+	raw, err := rep.Parameters.GetKeyValue("service")
+	if err != nil {
+		return nil, fmt.Errorf("rep.Parameters.GetKeyValue('service'): %v", err)
+	}
+
+	var s config.Service
+	err = raw.Interface(&s)
+	if err != nil {
+		return nil, fmt.Errorf("raw.Interface: %v", err)
+	}
+
+	return &s, nil
+}
+
+func (c *Client) ServiceByUrl(url string) (*config.Service, error) {
+	req := message.Request{
+		Command:    handler.ServiceByUrl,
+		Parameters: key_value.Empty().Set("url", url),
+	}
+
+	rep, err := c.socket.Request(&req)
+	if err != nil {
+		return nil, fmt.Errorf("socket.Request('%s'): %w", handler.ServiceByUrl, err)
+	}
+
+	if !rep.IsOK() {
+		return nil, fmt.Errorf("replied an error: %s", rep.Message)
+	}
+
+	raw, err := rep.Parameters.GetKeyValue("service")
+	if err != nil {
+		return nil, fmt.Errorf("rep.Parameters.GetKeyValue('service'): %v", err)
+	}
+
+	var s config.Service
+	err = raw.Interface(&s)
+	if err != nil {
+		return nil, fmt.Errorf("raw.Interface: %v", err)
+	}
+
+	return &s, nil
+}
+
+func (c *Client) SetService(s *config.Service) error {
+	req := message.Request{
+		Command:    handler.SetService,
+		Parameters: key_value.Empty().Set("service", s),
+	}
+
+	err := c.socket.Submit(&req)
+	if err != nil {
+		return fmt.Errorf("socket.Request('%s'): %w", handler.SetService, err)
+	}
+
+	return nil
+}
+
+// Exist checks whether the given parameter exists in the config
+func (c *Client) Exist(name string) (bool, error) {
+	req := message.Request{
+		Command:    handler.ParamExist,
+		Parameters: key_value.Empty().Set("name", name),
+	}
+
+	rep, err := c.socket.Request(&req)
+	if err != nil {
+		return false, fmt.Errorf("socket.Request('%s'): %w", handler.ParamExist, err)
+	}
+
+	if !rep.IsOK() {
+		return false, fmt.Errorf("replied an error: %s", rep.Message)
+	}
+
+	exist, err := rep.Parameters.GetBoolean("exist")
+	if err != nil {
+		return false, fmt.Errorf("rep.Parameters.GetKeyValue('service'): %v", err)
+	}
+
+	return exist, nil
+}
+
+// String parameter from config engine
+func (c *Client) String(name string) (string, error) {
+	req := message.Request{
+		Command:    handler.StringParam,
+		Parameters: key_value.Empty().Set("name", name),
+	}
+
+	rep, err := c.socket.Request(&req)
+	if err != nil {
+		return "", fmt.Errorf("socket.Request('%s'): %w", handler.StringParam, err)
+	}
+
+	if !rep.IsOK() {
+		return "", fmt.Errorf("replied an error: %s", rep.Message)
+	}
+
+	value, err := rep.Parameters.GetString("value")
+	if err != nil {
+		return "", fmt.Errorf("rep.Parameters.GetString('value'): %v", err)
+	}
+
+	return value, nil
+}
+
+// Uint64 parameter from config engine
+func (c *Client) Uint64(name string) (uint64, error) {
+	req := message.Request{
+		Command:    handler.Uint64Param,
+		Parameters: key_value.Empty().Set("name", name),
+	}
+
+	rep, err := c.socket.Request(&req)
+	if err != nil {
+		return 0, fmt.Errorf("socket.Request('%s'): %w", handler.Uint64Param, err)
+	}
+
+	if !rep.IsOK() {
+		return 0, fmt.Errorf("replied an error: %s", rep.Message)
+	}
+
+	value, err := rep.Parameters.GetUint64("value")
+	if err != nil {
+		return 0, fmt.Errorf("rep.Parameters.GetUint64('value'): %v", err)
+	}
+
+	return value, nil
+}
+
+// Bool parameter from config engine
+func (c *Client) Bool(name string) (bool, error) {
+	req := message.Request{
+		Command:    handler.BoolParam,
+		Parameters: key_value.Empty().Set("name", name),
+	}
+
+	rep, err := c.socket.Request(&req)
+	if err != nil {
+		return false, fmt.Errorf("socket.Request('%s'): %w", handler.BoolParam, err)
+	}
+
+	if !rep.IsOK() {
+		return false, fmt.Errorf("replied an error: %s", rep.Message)
+	}
+
+	value, err := rep.Parameters.GetBoolean("value")
+	if err != nil {
+		return false, fmt.Errorf("rep.Parameters.GetBoolean('value'): %v", err)
+	}
+
+	return value, nil
+}
---
+++ client/client_test.go
@@ -0,0 +1,204 @@
+package client
+
+import (
+	"github.com/ahmetson/config-lib"
+	"github.com/ahmetson/config-lib/app"
+	"github.com/ahmetson/config-lib/handler"
+	"github.com/ahmetson/log-lib"
+	"github.com/ahmetson/os-lib/path"
+	"gopkg.in/yaml.v3"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/ahmetson/common-lib/data_type/key_value"
+	"github.com/stretchr/testify/suite"
+)
+
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including a T() method which
+// returns the current testing orchestra
+type TestClientSuite struct {
+	suite.Suite
+	handler    *handler.Handler
+	logger     *log.Logger
+	client     *Client
+	execPath   string
+	serviceId  string
+	serviceUrl string
+}
+
+// Make sure that Account is set to five
+// before each test
+func (test *TestClientSuite) SetupTest() {
+	s := test.Require
+
+	logger, err := log.New("config_test", false)
+	s().NoError(err)
+	test.logger = logger
+
+	// Current directory
+	test.execPath, err = path.CurrentDir()
+	s().NoError(err)
+
+	// createYaml uses this serviceId and serviceUrl
+	test.serviceId = "id"
+	test.serviceUrl = "github.com/ahmetson/sample"
+
+	// app engine will load the yaml
+	test.createYaml(test.execPath, "app")
+
+	// The config handler
+	h, err := handler.New()
+	s().NoError(err)
+	test.handler = h
+
+	err = test.handler.Start()
+	s().NoError(err)
+	time.Sleep(time.Millisecond * 200) // wait a bit for initialization
+
+	// Client that will send requests to the config handler
+	c, err := New()
+	s().NoError(err)
+	test.client = c
+
+	// Make the file
+	test.handler.Engine.SetDefault(app.EnvConfigPath, test.execPath)
+	test.handler.Engine.SetDefault(app.EnvConfigName, "app")
+
+	// Creating some random config parameters to fetch
+	test.handler.Engine.Set("bool", true)
+	test.handler.Engine.Set("string", "hello world")
+	test.handler.Engine.Set("uint64", uint64(123))
+}
+
+func (test *TestClientSuite) TearDownTest() {
+	s := test.Require
+
+	s().NoError(test.handler.Close())
+	s().NoError(test.client.Close())
+
+	time.Sleep(time.Millisecond * 200) // wait a bit for closing threads
+
+	test.deleteYaml(test.execPath, "app")
+}
+
+func (test *TestClientSuite) createYaml(dir string, name string) {
+	s := test.Require
+
+	sampleService := config.Empty(test.serviceId, test.serviceUrl, config.IndependentType)
+	kv := key_value.Empty().Set("services", []interface{}{sampleService})
+
+	serviceConfig, err := yaml.Marshal(kv.Map())
+	s().NoError(err)
+
+	filePath := filepath.Join(dir, name+".yml")
+
+	f, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	s().NoError(err)
+	_, err = f.Write(serviceConfig)
+	s().NoError(err)
+
+	s().NoError(f.Close())
+}
+
+func (test *TestClientSuite) deleteYaml(dir string, name string) {
+	s := test.Require
+
+	filePath := filepath.Join(dir, name+".yml")
+
+	exist, err := path.FileExist(filePath)
+	s().NoError(err)
+
+	if !exist {
+		return
+	}
+
+	s().NoError(os.Remove(filePath))
+}
+
+// Test_10_ServiceById fetching service by id
+func (test *TestClientSuite) Test_10_ServiceById() {
+	s := test.Require
+
+	// No id parameter was given
+	_, err := test.client.Service("")
+	s().Error(err)
+
+	// Unknown service must return failure
+	_, err = test.client.Service("unknown")
+	s().Error(err)
+
+	// Successful request
+	service, err := test.client.Service(test.serviceId)
+	s().NoError(err)
+	s().NotNil(service)
+}
+
+// Test_11_ServiceByUrl fetching service by url
+func (test *TestClientSuite) Test_11_ServiceByUrl() {
+	s := test.Require
+
+	// No id parameter was given
+	service, err := test.client.ServiceByUrl(test.serviceUrl)
+	s().NoError(err)
+	s().NotNil(service)
+}
+
+// Test_12_SetService set a new service
+func (test *TestClientSuite) Test_12_SetService() {
+	s := test.Require
+
+	sampleService := config.Empty(test.serviceId+"_2", test.serviceUrl+"_2", config.IndependentType)
+
+	// No id parameter was given
+	err := test.client.SetService(sampleService)
+	s().NoError(err)
+
+	// Validate that service was set in the config
+	service, err := test.client.Service(test.serviceId + "_2")
+	s().NoError(err)
+	s().Equal(sampleService.Url, service.Url)
+}
+
+// Test_13_onExist check parameter exists or not
+func (test *TestClientSuite) Test_13_onExist() {
+	s := test.Require
+
+	param := "not_exist"
+
+	// No id parameter was given
+	exist, err := test.client.Exist(param)
+	s().NoError(err)
+	s().False(exist)
+
+	// This parameter set in the test setup, so must exist
+	exist, err = test.client.Exist("bool")
+	s().NoError(err)
+	s().True(exist)
+}
+
+// Test_14_GetParam returns the string, uint or boolean parameters from the config engine
+func (test *TestClientSuite) Test_14_GetParam() {
+	s := test.Require
+
+	// bool
+	value, err := test.client.Bool("bool")
+	s().NoError(err)
+	s().True(value)
+
+	valueStr, err := test.client.String("string")
+	s().NoError(err)
+	s().NotEmpty(valueStr)
+
+	valueUint64, err := test.client.Uint64("uint64")
+	s().NoError(err)
+	s().NotZero(valueUint64)
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestHandler(t *testing.T) {
+	suite.Run(t, new(TestClientSuite))
+}
