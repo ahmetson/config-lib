94b943a6eaeaa261eea48b63b8d5372c1b6395c5
Medet Ahmetson <ahmetson@zoho.com>
2023-09-29 18:23:18 +0700

    Remove engine Interface

--- interface.go
+++ interface.go
@@ -1,12 +1,1 @@
 package config
-
-import (
-	"github.com/ahmetson/datatype-lib/data_type/key_value"
-)
-
-// An Interface of the config engine.
-// The configuration is represented as a key-value database.
-type Interface interface {
-	SetDefaults(value key_value.KeyValue)
-	Exist(string) bool
-}
