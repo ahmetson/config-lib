e6829b27fb6178ef634dfee285d508aec199f85e
Medet Ahmetson <ahmetson@zoho.com>
2023-09-29 18:53:07 +0700

    Move file operations from app/app.go to app/file.go

--- app/app.go
+++ app/app.go
@@ -9,12 +9,6 @@ import (
 	"fmt"
 	"github.com/ahmetson/config-lib/engine"
 	"github.com/ahmetson/config-lib/service"
-	"github.com/ahmetson/datatype-lib/data_type/key_value"
-	"github.com/ahmetson/os-lib/arg"
-	"github.com/ahmetson/os-lib/path"
-	"gopkg.in/yaml.v3"
-	"os"
-	"path/filepath"
 )
 
 const (
@@ -43,175 +37,43 @@ type App struct {
 // - if, environment variable exists, then load it.
 // - if, a flag exists, then load it.
 func New(configEngine *engine.Dev) (*App, error) {
-	// default app is empty
-	execPath, err := path.CurrentDir()
-	if err != nil {
-		return nil, fmt.Errorf("path.CurrentDir: %w", err)
-	}
-
-	flagFileParams, fileExist, err := flagExist(execPath)
+	fileParams, fileExist, err := filePath(configEngine)
 	if err != nil {
 		return nil, fmt.Errorf("flagExist: %w", err)
 	}
 
 	// Load the configuration by flag parameter
 	if fileExist {
-		if err := makeConfigDir(flagFileParams); err != nil {
-			return nil, fmt.Errorf("app.makeConfigDir: %w", err)
-		}
-
-		filePath := fileParamsToPath(flagFileParams)
+		filePath := fileParamsToPath(fileParams)
 		app, err := read(filePath)
 		if err != nil {
-			return nil, fmt.Errorf("configEngine.Load: %w", err)
+			return nil, fmt.Errorf("read('%s'): %w", filePath, err)
 		}
 		app.filePath = filePath
 
 		return app, nil
 	}
 
-	setDefault(execPath, configEngine)
-	envFileParams, fileExist, err := envExist(configEngine)
-	if err != nil {
-		return nil, fmt.Errorf("envExist: %w", err)
+	if fileParams == nil {
+		return nil, fmt.Errorf("file parameter is nil")
 	}
-
-	// Load the configuration by environment parameter
-	if fileExist {
-		if err := makeConfigDir(envFileParams); err != nil {
-			return nil, fmt.Errorf("app.makeConfigDir: %w", err)
-		}
-		filePath := fileParamsToPath(envFileParams)
-
-		app, err := read(filePath)
-		if err != nil {
-			return nil, fmt.Errorf("configEngine.Load: %w", err)
-		}
-		app.filePath = filePath
-
-		return app, nil
-	}
-
-	app := &App{
-	}
-
 	// File doesn't exist, let's write it.
 	// Priority is the flag path.
 	// If the user didn't pass the flags, then use an environment path.
 	// The environment path will not be nil, since it will use the default path.
-	if flagFileParams == nil {
-		app.fileParams = envFileParams
-	}
-	if envFileParams == nil {
-		return nil, fmt.Errorf("envFileParams is nil")
-	}
-
-	if err := makeConfigDir(flagFileParams); err != nil {
+	if err := makeConfigDir(fileParams); err != nil {
 		return nil, fmt.Errorf("app.makeConfigDir: %w", err)
 	}
-	app.filePath = fileParamsToPath(app.fileParams)
+
+	app := &App{
+		filePath: fileParamsToPath(fileParams),
+	}
+
 	if err := app.write(); err != nil {
 		return nil, fmt.Errorf("app.write: %w", err)
 	}
 
 	return app, nil
-}
-
-// filePath must be absolute
-func read(filePath string) (*App, error) {
-	var appConfig App
-
-	info, err := os.Stat(filePath)
-	if err != nil {
-		if err == os.ErrNotExist {
-			return &appConfig, nil
-		}
-		return nil, fmt.Errorf("os.Stat('%s'): %w", filePath, err)
-	}
-
-	f, err := os.OpenFile(filePath, os.O_RDONLY, 0600)
-	if err != nil {
-		return nil, fmt.Errorf("os.OpenFile('%s'): %w", filePath, err)
-	}
-
-	buf := make([]byte, info.Size())
-	_, err = f.Read(buf)
-	// All this long piece of code is needed
-	// to catch the close error.
-	closeErr := f.Close()
-	if closeErr != nil {
-		if err != nil {
-			return nil, fmt.Errorf("%v: file.Close: %w", err, closeErr)
-		} else {
-			return nil, fmt.Errorf("file.Close: %w", closeErr)
-		}
-	} else if err != nil {
-		return nil, fmt.Errorf("file.Write: %w", err)
-	}
-
-	err = yaml.Unmarshal(buf, &appConfig)
-	if err != nil {
-		return nil, fmt.Errorf("yaml.Unmarshal: %w", err)
-	}
-	if appConfig.Services == nil {
-		appConfig.Services = make([]*service.Service, 0)
-	}
-	if appConfig.ProxyChains == nil {
-		appConfig.ProxyChains = make([]*service.ProxyChain, 0)
-	}
-
-	return &appConfig, nil
-}
-
-// flagExist checks is there any configuration flag.
-// If the configuration flag is set, it checks does it exist in the file system.
-func flagExist(execPath string) (key_value.KeyValue, bool, error) {
-	if !arg.FlagExist(service.ConfigFlag) {
-		return nil, false, nil
-	}
-
-	configPath := arg.FlagValue(service.ConfigFlag)
-
-	absPath := path.AbsDir(execPath, configPath)
-
-	exists, err := path.FileExist(absPath)
-	if err != nil {
-		return nil, false, fmt.Errorf("path.FileExists('%s'): %w", absPath, err)
-	}
-
-	if !exists {
-		return nil, false, fmt.Errorf("file (%s) not found", absPath)
-	}
-
-	dir, fileName := path.DirAndFileName(absPath)
-	return engine.YamlPathParam(dir, fileName), true, nil
-}
-
-// envExist checks is there any configuration file path from env.
-// If it exists, checks does it exist in the file system.
-//
-// In case if it doesn't exist, it will try to load the default configuration.
-func envExist(configEngine *engine.Dev) (key_value.KeyValue, bool, error) {
-	if !configEngine.Exist(EnvConfigName) || !configEngine.Exist(EnvConfigPath) {
-		return nil, false, nil
-	}
-
-	configName := configEngine.GetString(EnvConfigName)
-	configPath := configEngine.GetString(EnvConfigPath)
-	absPath := path.AbsDir(configPath, configName+".yml")
-	exists, err := path.FileExist(absPath)
-	if err != nil {
-		return nil, false, fmt.Errorf("path.FileExists('%s'): %w", absPath, err)
-	}
-
-	envPath := engine.YamlPathParam(configPath, configName)
-	return envPath, exists, nil
-}
-
-// setDefault paths of the local file to load by default
-func setDefault(execPath string, engine *engine.Dev) {
-	engine.SetDefault(EnvConfigName, "app")
-	engine.SetDefault(EnvConfigPath, execPath)
 }
 
 // Service by id returned from the app configuration.
@@ -254,75 +116,6 @@ func (a *App) SetService(s *service.Service) error {
 
 	if err := a.write(); err != nil {
 		return fmt.Errorf("app.write: %w", err)
-	}
-
-	return nil
-}
-
-func (a *App) createYaml() key_value.KeyValue {
-	var services = a.Services
-	kv := key_value.New()
-	kv.Set("services", services)
-
-	return kv
-}
-
-// makeConfigDir converts fileParams to the full file path.
-// if the configuration file is stored in the nested directory, then those directories are created.
-func makeConfigDir(fileParams key_value.KeyValue) error {
-	if fileParams == nil {
-		return fmt.Errorf("fileParams nil")
-	}
-	dirPath, err := fileParams.StringValue("configPath")
-	if err != nil {
-		return fmt.Errorf("a.fileParams.StringValue('configPath'): %w", err)
-	}
-
-	dirExist, err := path.DirExist(dirPath)
-	if err != nil {
-		return fmt.Errorf("path.DirExist('%s'): %w", dirPath, err)
-	}
-	if !dirExist {
-		err = path.MakeDir(dirPath)
-		if err != nil {
-			return fmt.Errorf("path.MakeDir('%s'): %w", dirPath, err)
-		}
-	}
-
-	return nil
-}
-
-func fileParamsToPath(fileParams key_value.KeyValue) string {
-	name, _ := fileParams.StringValue("name")
-	dirPath, _ := fileParams.StringValue("configPath")
-	return filepath.Join(dirPath, name+".yml")
-}
-
-// Writes the service as the yaml on the given path.
-// If the path doesn't contain the file extension, it will through an error
-func (a *App) write() error {
-	kv := a.createYaml()
-
-	appConfig, err := yaml.Marshal(kv.Map())
-	if err != nil {
-		return fmt.Errorf("yaml.Marshal: %w", err)
-	}
-
-	f, err := os.OpenFile(a.filePath, os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		return fmt.Errorf("os.OpenFile('%s'): %w", a.filePath, err)
-	}
-
-	_, err = f.Write(appConfig)
-	closeErr := f.Close()
-	if closeErr != nil {
-		if err != nil {
-			return fmt.Errorf("%v: file.Close: %w", err, closeErr)
-		} else {
-			return fmt.Errorf("file.Close: %w", closeErr)
-		}
-	} else if err != nil {
-		return fmt.Errorf("file.Write: %w", err)
 	}
 
 	return nil
---
+++ app/file.go
@@ -0,0 +1,242 @@
+package app
+
+import (
+	"fmt"
+	"github.com/ahmetson/config-lib/engine"
+	"github.com/ahmetson/config-lib/service"
+	"github.com/ahmetson/datatype-lib/data_type/key_value"
+	"github.com/ahmetson/os-lib/arg"
+	"github.com/ahmetson/os-lib/path"
+	"gopkg.in/yaml.v3"
+	"os"
+	"path/filepath"
+)
+
+//
+// Yaml file operations
+//
+
+// The filePath returns the file path.
+// First it reads from a flag, then from environment variable.
+// Lastly, read the default file.
+func filePath(configEngine *engine.Dev) (key_value.KeyValue, bool, error) {
+	// default app is empty
+	execPath, err := path.CurrentDir()
+	if err != nil {
+		return nil, false, fmt.Errorf("path.CurrentDir: %w", err)
+	}
+
+	flagFileParams, fileExist, err := flagExist(execPath)
+	if err != nil {
+		return nil, false, fmt.Errorf("flagExist: %w", err)
+	}
+
+	// Load the configuration by flag parameter
+	if fileExist {
+		//if err := makeConfigDir(flagFileParams); err != nil {
+		//	return nil, fmt.Errorf("app.makeConfigDir: %w", err)
+		//}
+
+		return flagFileParams, true, nil
+	}
+
+	setDefault(execPath, configEngine)
+	envFileParams, fileExist, err := envExist(configEngine)
+	if err != nil {
+		return nil, true, fmt.Errorf("envExist: %w", err)
+	}
+
+	// Load the configuration by environment parameter
+	if fileExist {
+		//if err := makeConfigDir(envFileParams); err != nil {
+		//	return nil, fmt.Errorf("app.makeConfigDir: %w", err)
+		//}
+
+		return envFileParams, true, nil
+	}
+
+	// flag is priority, if not given then
+	fileParams := flagFileParams
+	if flagFileParams == nil {
+		fileParams = envFileParams
+	}
+	if fileParams == nil {
+		return nil, false, fmt.Errorf("file parameter is nil")
+	}
+
+	// File doesn't exist, let's write it.
+	// Priority is the flag path.
+	// If the user didn't pass the flags, then use an environment path.
+	// The environment path will not be nil, since it will use the default path.
+	if err := makeConfigDir(fileParams); err != nil {
+		return nil, false, fmt.Errorf("app.makeConfigDir: %w", err)
+	}
+
+	return fileParams, false, nil
+}
+
+// filePath must be absolute
+func read(filePath string) (*App, error) {
+	var appConfig App
+
+	info, err := os.Stat(filePath)
+	if err != nil {
+		if err == os.ErrNotExist {
+			return &appConfig, nil
+		}
+		return nil, fmt.Errorf("os.Stat('%s'): %w", filePath, err)
+	}
+
+	f, err := os.OpenFile(filePath, os.O_RDONLY, 0600)
+	if err != nil {
+		return nil, fmt.Errorf("os.OpenFile('%s'): %w", filePath, err)
+	}
+
+	buf := make([]byte, info.Size())
+	_, err = f.Read(buf)
+	// All this long piece of code is needed
+	// to catch the close error.
+	closeErr := f.Close()
+	if closeErr != nil {
+		if err != nil {
+			return nil, fmt.Errorf("%v: file.Close: %w", err, closeErr)
+		} else {
+			return nil, fmt.Errorf("file.Close: %w", closeErr)
+		}
+	} else if err != nil {
+		return nil, fmt.Errorf("file.Write: %w", err)
+	}
+
+	err = yaml.Unmarshal(buf, &appConfig)
+	if err != nil {
+		return nil, fmt.Errorf("yaml.Unmarshal: %w", err)
+	}
+	if appConfig.Services == nil {
+		appConfig.Services = make([]*service.Service, 0)
+	}
+	if appConfig.ProxyChains == nil {
+		appConfig.ProxyChains = make([]*service.ProxyChain, 0)
+	}
+
+	return &appConfig, nil
+}
+
+// flagExist checks is there any configuration flag.
+// If the configuration flag is set, it checks does it exist in the file system.
+func flagExist(execPath string) (key_value.KeyValue, bool, error) {
+	if !arg.FlagExist(service.ConfigFlag) {
+		return nil, false, nil
+	}
+
+	configPath := arg.FlagValue(service.ConfigFlag)
+
+	absPath := path.AbsDir(execPath, configPath)
+
+	exists, err := path.FileExist(absPath)
+	if err != nil {
+		return nil, false, fmt.Errorf("path.FileExists('%s'): %w", absPath, err)
+	}
+
+	if !exists {
+		return nil, false, fmt.Errorf("file (%s) not found", absPath)
+	}
+
+	dir, fileName := path.DirAndFileName(absPath)
+	return engine.YamlPathParam(dir, fileName), true, nil
+}
+
+// envExist checks is there any configuration file path from env.
+// If it exists, checks does it exist in the file system.
+//
+// In case if it doesn't exist, it will try to load the default configuration.
+func envExist(configEngine *engine.Dev) (key_value.KeyValue, bool, error) {
+	if !configEngine.Exist(EnvConfigName) || !configEngine.Exist(EnvConfigPath) {
+		return nil, false, nil
+	}
+
+	configName := configEngine.GetString(EnvConfigName)
+	configPath := configEngine.GetString(EnvConfigPath)
+	absPath := path.AbsDir(configPath, configName+".yml")
+	exists, err := path.FileExist(absPath)
+	if err != nil {
+		return nil, false, fmt.Errorf("path.FileExists('%s'): %w", absPath, err)
+	}
+
+	envPath := engine.YamlPathParam(configPath, configName)
+	return envPath, exists, nil
+}
+
+// setDefault paths of the local file to load by default
+func setDefault(execPath string, engine *engine.Dev) {
+	engine.SetDefault(EnvConfigName, "app")
+	engine.SetDefault(EnvConfigPath, execPath)
+}
+
+func (a *App) createYaml() key_value.KeyValue {
+	var services = a.Services
+	kv := key_value.New()
+	kv.Set("services", services)
+
+	return kv
+}
+
+// makeConfigDir converts fileParams to the full file path.
+// if the configuration file is stored in the nested directory, then those directories are created.
+func makeConfigDir(fileParams key_value.KeyValue) error {
+	if fileParams == nil {
+		return fmt.Errorf("fileParams nil")
+	}
+	dirPath, err := fileParams.StringValue("configPath")
+	if err != nil {
+		return fmt.Errorf("a.fileParams.StringValue('configPath'): %w", err)
+	}
+
+	dirExist, err := path.DirExist(dirPath)
+	if err != nil {
+		return fmt.Errorf("path.DirExist('%s'): %w", dirPath, err)
+	}
+	if !dirExist {
+		err = path.MakeDir(dirPath)
+		if err != nil {
+			return fmt.Errorf("path.MakeDir('%s'): %w", dirPath, err)
+		}
+	}
+
+	return nil
+}
+
+func fileParamsToPath(fileParams key_value.KeyValue) string {
+	name, _ := fileParams.StringValue("name")
+	dirPath, _ := fileParams.StringValue("configPath")
+	return filepath.Join(dirPath, name+".yml")
+}
+
+// Writes the service as the yaml on the given path.
+// If the path doesn't contain the file extension, it will through an error
+func (a *App) write() error {
+	kv := a.createYaml()
+
+	appConfig, err := yaml.Marshal(kv.Map())
+	if err != nil {
+		return fmt.Errorf("yaml.Marshal: %w", err)
+	}
+
+	f, err := os.OpenFile(a.filePath, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return fmt.Errorf("os.OpenFile('%s'): %w", a.filePath, err)
+	}
+
+	_, err = f.Write(appConfig)
+	closeErr := f.Close()
+	if closeErr != nil {
+		if err != nil {
+			return fmt.Errorf("%v: file.Close: %w", err, closeErr)
+		} else {
+			return fmt.Errorf("file.Close: %w", closeErr)
+		}
+	} else if err != nil {
+		return fmt.Errorf("file.Write: %w", err)
+	}
+
+	return nil
+}
